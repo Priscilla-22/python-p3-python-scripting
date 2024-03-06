3d64b37ab09e757176cbf28728b61a855fba37ca
Priscilla-22 <priscillamw123@gmail.com>
2024-03-06 08:30:33 +0300

    feat: python scripting

--- bin/script.py
+++ bin/script.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""import sys
+# The user input starts at index 1 in the sys.argv list.
+name = sys.argv[1]
+if __name__ == "__main__":
+    print(f"The name is {name} ")"""
+
 import os
 if __name__ == "__main__":
     os.system('ls -l')
+
